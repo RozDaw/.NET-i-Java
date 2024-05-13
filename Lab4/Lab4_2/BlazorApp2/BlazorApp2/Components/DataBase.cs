1df0e7f80d9e9ebeb89e2f7d05ac49d76c432816
RozDaw <103137692+RozDaw@users.noreply.github.com>
2024-05-13 17:16:41 +0200

    lab4

---
+++ Lab4/Lab4_2/BlazorApp2/BlazorApp2/Components/DataBase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp2.Components
+{
+    internal class DataBasee : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        public DataBasee()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite(@" Data Source=Univ.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie() { Id = 1, Title = "Gorzow" },
+                new Movie() { Id = 2, Title = "Krakow" },
+                new Movie() { Id = 3, Title = "Gdansk" }
+                );
+        }
+
+
+    }
+}
---
+++ Lab4/Lab4_2/BlazorApp2/BlazorApp2/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BlazorApp2.Components;
+
+namespace BlazorApp2.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<BlazorApp2.Components.Movie> Movie { get; set; } = default!;
+    }
+}
